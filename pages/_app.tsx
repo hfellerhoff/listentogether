a4b64157faa58ca4bfeef7006121cad587d92b57
Henry Fellerhoff <henryfellerhoff@gmail.com>
2022-06-09 04:38:37 -0400

    chore: remove imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import '../styles/globals.css';
 import { Provider } from 'jotai';
-import useSpotifyWebPlayback from '../hooks/spotify/useSpotifyWebPlayback';
 
 import { globalStyles } from '../stitches.config';
 import StitchesThemeController from '../components/StitchesThemeController';
 import { ThemeProvider } from 'next-themes';
-import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
   globalStyles();
