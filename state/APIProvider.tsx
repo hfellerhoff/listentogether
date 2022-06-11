24ddf2e814d55bf8b451ae26d5358f4a9e39abf8
Henry Fellerhoff <henryfellerhoff@gmail.com>
2022-06-11 00:11:16 -0500

    fix: unimplemented context

--- state/APIProvider.tsx
+++ state/APIProvider.tsx
@@ -1,44 +1,45 @@
-import { useEffect, useState, createContext, useContext } from 'react';
-import { API } from '../lib/api';
+export {};
+// import { useEffect, useState, createContext, useContext } from 'react';
+// import { API } from '../lib/api';
 
-export const APIContext = createContext<{
-  api: API;
-}>({
-  api: null,
-});
+// export const APIContext = createContext<{
+//   api: API;
+// }>({
+//   api: null,
+// });
 
-export const UserContextProvider = (props: any) => {
-  const [session, setSession] = useState<Session | null>(null);
-  const [user, setUser] = useState<User | null>(null);
+// export const UserContextProvider = (props: any) => {
+//   const [session, setSession] = useState<Session | null>(null);
+//   const [user, setUser] = useState<User | null>(null);
 
-  useEffect(() => {
-    const session = supabase.auth.session();
-    setSession(session);
-    setUser(session?.user ?? null);
-    const { data: authListener } = supabase.auth.onAuthStateChange(
-      async (event, session) => {
-        setSession(session);
-        setUser(session?.user ?? null);
-      }
-    );
+//   useEffect(() => {
+//     const session = supabase.auth.session();
+//     setSession(session);
+//     setUser(session?.user ?? null);
+//     const { data: authListener } = supabase.auth.onAuthStateChange(
+//       async (event, session) => {
+//         setSession(session);
+//         setUser(session?.user ?? null);
+//       }
+//     );
 
-    return () => {
-      authListener!.unsubscribe();
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+//     return () => {
+//       authListener!.unsubscribe();
+//     };
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, []);
 
-  const value = {
-    session,
-    user,
-  };
-  return <UserContext.Provider value={value} {...props} />;
-};
+//   const value = {
+//     session,
+//     user,
+//   };
+//   return <UserContext.Provider value={value} {...props} />;
+// };
 
-export const useUser = () => {
-  const context = useContext(UserContext);
-  if (context === undefined) {
-    throw new Error(`useUser must be used within a UserContextProvider.`);
-  }
-  return context;
-};
+// export const useUser = () => {
+//   const context = useContext(UserContext);
+//   if (context === undefined) {
+//     throw new Error(`useUser must be used within a UserContextProvider.`);
+//   }
+//   return context;
+// };
