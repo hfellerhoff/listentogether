360f7277f8e9db54cac17894a4b6416408b23338
Henry Fellerhoff <henryfellerhoff@gmail.com>
2022-06-11 00:13:58 -0500

    fix: build error

--- state/apiAtom.ts
+++ state/apiAtom.ts
@@ -2,4 +2,4 @@ import { atom } from 'jotai';
 import { API } from '../lib/api';
 import Service from '../models/Service';
 
-export const apiAtom = atom<API>(new API(Service.Spotify));
+export const apiAtom = atom<API>(new API());
