1aa81ab01a39f5debd9b3c27ed4d53c4e90bef1f
Henry Fellerhoff <henryfellerhoff@gmail.com>
2022-06-12 18:31:55 -0500

    chore: remove unused code

--- lib/api.ts
+++ lib/api.ts
@@ -1,13 +1,5 @@
-// import Spotify from 'spotify-web-api-js';
-
 import ListenTogetherAPI from './listen-together';
 import SpotifyAPI from './spotify';
-
-export type ServiceAPI = {
-  Routes: Record<string, string>;
-  Cookies: Record<string, string>;
-  Secrets: Record<string, string>;
-};
 
 const API = {
   ListenTogether: ListenTogetherAPI,
--- lib/listen-together.ts
+++ lib/listen-together.ts
@@ -1,14 +1,9 @@
-import { ServiceAPI } from './api';
-
 export const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
     : process.env.VERCEL_ENV === 'preview'
     ? process.env.VERCEL_URL
     : 'https://www.listentogether.app';
-
-export const API_SPOTIFY_AUTH_REDIRECT_URI = `${BASE_URL}/api/spotify/callback`;
-export const API_SPOTIFY_AUTH_CALLBACK_URI = `${BASE_URL}/dashboard?`;
 
 export const buildListenTogetherAPIUrl = (url: string) => `${BASE_URL}${url}`;
 
