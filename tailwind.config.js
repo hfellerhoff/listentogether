5372d40f89d09a95758db6e63292859853264ada
hfellerhoff <henryfellerhoff@gmail.com>
2023-02-04 11:03:10 -0500

    add eslint, tailwind and update packages

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx}',
+    './lib/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
